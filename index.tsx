cfe935628f65a824f2498f9c6794a02ae8811e77
vertiljivenson9 <vertiljivenson9@gmail.com>
2025-12-30 12:52:44 -0400

    Sovereign Sync: index.tsx [retroOS-Vertil]

--- index.tsx
+++ index.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error("Root not found");
+const root = ReactDOM.createRoot(rootElement);
 root.render(<React.StrictMode><App /></React.StrictMode>);
