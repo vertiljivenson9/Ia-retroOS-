13d57375d7fb950cf36d77bf05af0b0e20ed4873
vertiljivenson9 <vertiljivenson9@gmail.com>
2025-12-30 16:51:25 -0400

    retroOS Neural Mesh: index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Root not found");
-const root = ReactDOM.createRoot(rootElement);
-root.render(<React.StrictMode><App /></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode><App /></React.StrictMode>
+);
